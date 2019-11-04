a272cd550bda208112448f74cd0d2e94471ea44b
LuK1337 <priv.luk@gmail.com>
2019-11-04 18:44:48 +0100

    sm8150-common: Finish up QCOM WFD
    Change-Id: If7317d71c8af2957892366af0a9c55dc0c644412

--- common.mk
+++ common.mk
@@ -226,6 +226,7 @@ PRODUCT_PACKAGES += \
     hwcomposer.msmnile \
     libdisplayconfig \
     libdisplayconfig.vendor \
+    libqdMetaData \
     libqdMetaData.system \
     libtinyxml \
     libvulkan \
@@ -500,8 +501,8 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     libnl
 
-#PRODUCT_BOOT_JARS += \
-#    WfdCommon
+PRODUCT_BOOT_JARS += \
+    WfdCommon
 
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     ro.surface_flinger.force_hwc_copy_for_virtual_displays=true \
